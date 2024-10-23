d810eee90ed50934d585827710ecd2b1a5fcbc41
Cyrus Chan <cyruschan1212@gmail.com>
2024-10-23 16:12:59 +0800

    Complete drawer layout

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
@@ -8,10 +9,28 @@ class HomePage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(title: const Text("Foo")),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.favorite_border)),
-        drawer: const Drawer(),
+          onPressed: () {}, child: const Icon(Icons.favorite_border)),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(12),
+                child: IconButton(
+                    onPressed: () {
+                      Navigator.pop<void>(context);
+                    },
+                    icon: const Icon(Icons.arrow_back))),
+            ListTile(
+                leading: const Icon(FontAwesomeIcons.clock),
+                title: const Text("Reward history"),
+                onTap: () {}),
+            ListTile(
+                leading: const Icon(FontAwesomeIcons.locationDot),
+                title: const Text("Find recycle bin"),
+                onTap: () {})
+          ],
+        ),
+      ),
     );
   }
-  
 }
