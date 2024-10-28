11479af4ec50ce7ccb5c412e143c88e6bfc5d56a
Cyrus Chan <cyruschan1212@gmail.com>
2024-10-28 11:48:01 +0800

    Implement data archive controller

--- app/lib/model/user_infos/session.dart
+++ app/lib/model/user_infos/session.dart
@@ -3,6 +3,8 @@ import '../identifiable.dart';
 final class Session implements Identifiable<String> {
   @override
   final String identifier;
+  final DateTime createAt;
 
-  const Session(this.identifier);
+  Session(this.identifier, [DateTime? createAt])
+      : createAt = createAt ?? DateTime.now();
 }
