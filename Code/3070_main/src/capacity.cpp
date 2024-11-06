9589b98d4ec3a0586ddda5eec726bbd4e260cfc3
ZTHHK0313 <101486797+ZTHHK0313@users.noreply.github.com>
2024-11-06 17:09:33 +0800

    updated

--- Code/3070_main/src/capacity.cpp
+++ Code/3070_main/src/capacity.cpp
@@ -1,17 +1,100 @@
-#include <Arduino.h>
 #include "capacity.h"
-int scaledValue = 0;
 
-void dectectcap() {
-    // Read the analog value from the photoresistor
-    int sensorValue = analogRead(photoResistorPin);
-
-    // Map the sensor value to a scale of 0 to 100
-    scaledValue = map(sensorValue, 0, adcMaxValue, 0, 100);
-
-    // Invert the value so 0 is lightest and 100 is darkest
-    scaledValue = 100 - scaledValue;
-
-
-    delay(1000);  // Wait for a second before the next reading
+float getLDRPercentage(int pin) {
+  int ldrValue = analogRead(pin);
+  // Assuming the range is 0 to 1023, map it to a percentage (0 to 100%)
+  float percentage = (ldrValue / 1023.0) * 100;
+  return percentage;
 }
+
+
+void DisplayCapacity() {
+  
+  bool isFull[] = {false, false, false};
+
+  // Read values from all LDRs and calculate percentages
+  float canPercentage = getLDRPercentage(ldrPins[0]);
+  float plasticPercentage = getLDRPercentage(ldrPins[1]);
+  float paperPercentage = getLDRPercentage(ldrPins[2]);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+
+  display.println("Occupied Capacity:");
+
+  if (canPercentage < fullCapacity) {
+    display.println("Can: FULL");
+  } else {
+    display.println("Can: Available");
+  }
+
+  if (plasticPercentage < fullCapacity) {
+    display.println("Plastic: FULL");
+  } else {
+    display.println("Plastic: Available");
+  }
+
+  if (paperPercentage < fullCapacity) {
+    display.println("Paper: FULL");
+  } else {
+    display.println("Paper: Available");
+  }
+
+  display.display();
+}
+
+// testing
+// float getLDRPercentage(int pin) {
+//   int ldrValue = analogRead(pin);
+//   // Assuming the range is 0 to 1023, map it to a percentage (0 to 100%)
+//   float percentage = ldrValue;
+//   return percentage;
+// }
+// void DisplayCapacity() {
+  
+//   bool isFull[] = {false, false, false};
+
+//   // Read values from all LDRs and calculate percentages
+//   float canPercentage = getLDRPercentage(ldrPins[0]);
+//   float plasticPercentage = getLDRPercentage(ldrPins[1]);
+//   float paperPercentage = getLDRPercentage(ldrPins[2]);
+
+//   display.clearDisplay();
+//   display.setTextSize(1);
+//   display.setTextColor(SSD1306_WHITE);
+//   display.setCursor(0, 10);
+
+//   display.println("Occupied Capacity:");
+
+//   display.print("Can: ");
+//   display.println(String(canPercentage) + "%");
+
+//       display.print("Plastic: ");
+//     display.println(String(plasticPercentage) + "%");
+
+//     display.print("Paper: ");
+//     display.println(String(paperPercentage) + "%");
+
+  
+//   display.display();
+// }
+
+// void setup() {
+//   Serial.begin(9600);
+//   SPI.begin();  
+
+//   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+//     Serial.println(F("SSD1306 allocation failed"));
+//     for (;;);
+//   }
+//   display.clearDisplay();
+//   display.display(); 
+//   DisplayCapacity();
+// }
+
+// void loop() {
+//   DisplayCapacity();
+// }
+
---
+++ Code/3070_main/src/card_and_servo_function.cpp
@@ -0,0 +1,215 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include <Servo.h>
+#include "card_and_servo_setting.h"
+
+Servo coverServo;  // Create a servo object
+
+struct Card {
+  int id;
+  byte uid[4];
+  int count;
+};
+
+int curr_cards = 0;  
+Card cards[MAX_CARDS];  
+
+byte scannedUID[4];  
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void printUID(byte *uid);
+void DisplayCapacity();
+int searchCardByUID(byte uid[4]);
+bool addCard(int id, byte uid[4]);
+int wasteClassify();
+void moveCoverServo();
+void updateBinCapacity(int wcase);
+void displayCardInfo(int index);
+void classifyResult(int wcase);
+void DisplayMessage(const char *message, int duration);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();  
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  display.clearDisplay();
+  display.display();
+
+  coverServo.attach(SERVO_PIN);  // Attach the servo to the defined pin
+  coverServo.write(0);  // Set initial servo position to 0 degrees
+
+  DisplayCapacity();
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  memcpy(scannedUID, rfid.uid.uidByte, 4);
+  printUID(scannedUID);  
+
+  moveCoverServo();  // Always open the cover when a card is detected
+
+  int cardIndex = searchCardByUID(scannedUID);
+  if (cardIndex == -1) {
+    addCard(curr_cards + 1, scannedUID);
+  } else {
+    int result = wasteClassify(); // Capture the classification result
+
+    if (result != 0) {
+      cards[cardIndex].count++;
+      updateBinCapacity(result);
+      displayCardInfo(cardIndex);
+    }
+  }
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+  DisplayCapacity();
+}
+
+void printUID(byte *uid) {
+  for (byte i = 0; i < 4; i++) {
+    Serial.print(uid[i], DEC);
+    if (i != 3) Serial.print(", ");
+  }
+  Serial.println();
+}
+
+bool addCard(int id, byte uid[4]) {
+  if (curr_cards >= MAX_CARDS) {  
+    DisplayMessage("Error: Card DB Full", 2000);
+    return false;
+  }
+
+  memcpy(cards[curr_cards].uid, uid, 4);  
+  cards[curr_cards].id = id;
+  cards[curr_cards].count = 1;  
+  
+  int result = wasteClassify();
+  if (result != 0){
+    updateBinCapacity(result);
+    displayCardInfo(curr_cards);
+    curr_cards++;
+    return true;
+  }
+  return false;
+}
+
+int searchCardByUID(byte uid[4]) {
+  for (int i = 0; i < curr_cards; i++) {
+    if (memcmp(uid, cards[i].uid, 4) == 0) return i;
+  }
+  return -1;  
+}
+
+void displayCardInfo(int index) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+  display.print("Registered ID: ");
+  display.println(cards[index].id);
+  display.print("Points: ");
+  display.println(cards[index].count);
+  display.display();
+  delay(1000);
+}
+
+void DisplayMessage(const char *message, int duration) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+  display.println(message);
+  display.display();
+  delay(duration);
+}
+
+void moveCoverServo() {
+  coverServo.write(90);  // Move the servo to 90 degrees
+  DisplayMessage("Cover is opened", 3000);  
+  delay(1000);
+  coverServo.write(0);   // Return the servo to 0 degrees
+}
+
+int wcase = 3; // testing
+int wasteClassify() { // testing
+  DisplayMessage("Analyzing...", 2000);
+
+  classifyResult(wcase);
+
+  if (wcase == 1 || wcase == 2 || wcase == 3) {
+    return wcase;
+  } else {
+    return 0;
+  }
+}
+
+void classifyResult(int wcase) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+
+  if (wcase == 1) {
+    display.println("Waste has entered:");
+    display.println("Can recycling bin");
+  } else if (wcase == 2) {
+    display.println("Waste has entered:");
+    display.println("Plastic recycling bin");
+  } else if (wcase == 3) {
+    display.println("Waste has entered:");
+    display.println("Paper recycling bin");
+  } else {
+    display.println("Unrecognized Waste");
+    display.println("Please take back the waste");
+  }
+  display.display();
+  delay(2000);
+}
+
+float canPercentage = 0;   // testing
+float plasticPercentage = 0;  // testing
+float paperPercentage = 100;  // testing
+
+void updateBinCapacity(int wcase) {
+  float increase = 5.0;
+
+  if (wcase == 1) {
+    canPercentage += increase;
+  } else if (wcase == 2) {
+    plasticPercentage += increase;
+  } else if (wcase == 3) {
+    paperPercentage += increase;
+  }
+}
+
+const float fullCapacity = 90.0;
+void DisplayCapacity() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+
+  display.println("Occupied Capacity:");
+  display.print("Can: ");
+  display.println(canPercentage < fullCapacity ? String(canPercentage) + "%" : "FULL");
+
+  display.print("Plastic: ");
+  display.println(plasticPercentage < fullCapacity ? String(plasticPercentage) + "%" : "FULL");
+
+  display.print("Paper: ");
+  display.println(paperPercentage < fullCapacity ? String(paperPercentage) + "%" : "FULL");
+
+  display.display();
+}
