f0566c59030c288e0c760047a54dfa8cdb2cb7c0
ZTHHK0313 <101486797+ZTHHK0313@users.noreply.github.com>
2024-11-06 18:37:55 +0800

    delete watse code

--- Code/3070_main/src/capacity.h
+++ Code/3070_main/src/capacity.h
@@ -3,10 +3,5 @@
 #include <Adafruit_GFX.h> 
 #include <Adafruit_SSD1306.h>
 
-#define SS_PIN 53
-#define RST_PIN 5
-#define SCREEN_WIDTH 128  
-#define SCREEN_HEIGHT 64  
-#define OLED_RESET -1 
 const int ldrPins[] = {A0, A1, A2};
 float getLDRPercentage(int pin);
