74c6f23794afada970025398dd3fc25fb6520aec
Cyrus Chan <cyruschan1212@gmail.com>
2024-11-20 18:05:24 +0800

    Uses api layout

--- app/lib/controller/recycle_bin.dart
+++ app/lib/controller/recycle_bin.dart
@@ -38,13 +38,14 @@ Future<List<RecycleBinTuple>> loadBookmarkedRecycleBins(User usr) async {
   final List<RecycleBinTuple> tmp = [];
   final Map<String, dynamic> rbCap = await getAllRawRecycleBinCapacity();
 
-  if (favLocs.where((loc) => loc.identifier == 1).isNotEmpty) {
-    var currentLoc = favLocs.first;
+  if (favLocs.where((loc) => loc.identifier == rbCap["id"]).isNotEmpty) {
+    final currentLoc = favLocs.first;
+    final capacity = rbCap["capacity"];
 
     tmp.add((
       currentLoc,
-      RemainCapacity(currentLoc.identifier, rbCap["plastic"], rbCap["metal"],
-          rbCap["paper"])
+      RemainCapacity(currentLoc.identifier, capacity["plastic"], capacity["metal"],
+          capacity["paper"])
     ));
   }
 
@@ -69,7 +70,7 @@ Future<Map<String, dynamic>> getAllRawRecycleBinCapacity() async {
   final RestClient c = RestClient();
 
   try {
-    return await c.get(APIPath.capacity).then((resp) => jsonDecode(resp.body)["capacity"]);
+    return await c.get(APIPath.capacity).then((resp) => jsonDecode(resp.body));
   } finally {
     c.close();
   }
--- app/lib/net/url.dart
+++ app/lib/net/url.dart
@@ -1,8 +1,8 @@
 abstract final class APIPath {
   const APIPath._();
 
-  // TODO: Uses 192.168.1.2 for physical interaction or 10.0.2.2:3000 when using emulator + Mockoon
-  static final Uri gateway = Uri.http(/*"192.168.1.2"*/"10.0.2.2:3000");
+  // TODO: Uses 192.168.1.156 for physical interaction or 10.0.2.2:3000 when using emulator + Mockoon
+  static final Uri gateway = Uri.http("192.168.1.156"/*"10.0.2.2:3000"*/);
 
   static final Uri userData = gateway.resolve("/api/user_data");
 
